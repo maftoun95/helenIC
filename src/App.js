e418f048821d7a3dbf3f9f1f5eb421fbdd8deee9
Saman M <maftoun95@gmail.com>
2017-08-20 09:11:18 -0700

    project set up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Content />
+      </div>
+    );
+    // return (
+    //   <div>
+    //     <Navbar/>
+    //     <Intro/>
+    //     <Content/>
+    //     <Footer/>
+    //   </div>
+    // );
+  }
+}
+
+export default App;
